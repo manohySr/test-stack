ae217679d3767919d596d459a3232275eed9cdb0
manohySr <manohyrajaonah@gmail.com>
2024-11-18 19:52:31 +0400

    add:   Read all CRUD

--- server/src/models/tasks.model.js
+++ server/src/models/tasks.model.js
@@ -1,4 +1,31 @@
 const Task = require("./tasks.mongo.js");
+async function findAllTask(priority, completed) {
+  try {
+    // Filter depending on the args
+    let filter = {};
+
+    if (priority) {
+      filter.priority = priority;
+    }
+
+    if (completed !== undefined) {
+      filter.completed = completed;
+    }
+
+    console.log("Filter:", filter);
+
+    // Fetch tasks based on the filter
+    const tasks = await Task.find(filter, {
+      _id: 0,
+      __v: 0,
+    });
+
+    return tasks;
+  } catch (error) {
+    console.error("Error finding tasks:", error);
+    throw new Error("Failed to fetch tasks. Please try again.");
+  }
+}
 async function createTask(task) {
   try {
     const newTask = await Task.create(task);
