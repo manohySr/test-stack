3aea17321b120af66ce337c72424a14683612248
manohySr <manohyrajaonah@gmail.com>
2024-11-19 21:04:40 +0400

    add:    generic button

---
+++ client/src/components/ui/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: "button",
+  },
+});
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="mt-5 bg-blue-500 text-white p-3 rounded hover:bg-blue-600 w-full md:w-64"
+  >
+    <slot></slot>
+  </button>
+</template>
