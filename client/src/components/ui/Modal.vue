d3abe418f61238d4873f7d7f6cb8320d3e5ad8a0
manohySr <manohyrajaonah@gmail.com>
2024-11-20 14:19:43 +0400

    add:   Modal component

---
+++ client/src/components/ui/Modal.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["close"]);
+
+const closeModal = () => {
+  emit("close");
+};
+</script>
+<template>
+  <div
+    v-if="isVisible"
+    class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+    @click.self="closeModal"
+  >
+    <div class="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full relative">
+      <button
+        @click="closeModal"
+        class="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+      >
+        ✕
+      </button>
+      <slot />
+    </div>
+  </div>
+</template>
