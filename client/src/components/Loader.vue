f4577baa9368b3958fd269a0f8b849e7cd276b00
manohySr <manohyrajaonah@gmail.com>
2024-11-22 12:41:45 +0400

    add:    Loader as a component

---
+++ client/src/components/Loader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50"
+  >
+    <div
+      class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"
+    ></div>
+  </div>
+</template>
